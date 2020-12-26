79180e784519459b8381e7a10e923e838d0583dc
shaxboz <shaxbozurinov1993@gmail.com>
2020-12-26 14:30:23 +0500

    we add Modal

--- js/script.js
+++ js/script.js
@@ -98,5 +98,38 @@ window.addEventListener('DOMContentLoaded', () =>{
     }
     setClock('.timer', deadline);
 
+    // MODAL
 
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    
+    modalTrigger.forEach(btn => {                     // trigger 2 ta bugani uchun forEach ishlatildi
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            //modal.classList.toggle('show');           // 2-usul modalni ochib yopishga
+            document.body.style.overflow = 'hidden';  // modal click qilinganda orqa fon scroll bo'lmasligi uchun
+        });
+    });
+    
+    function closeModal () {                        // DRY (2 ta joyda bir xil narsani ishlatmaslig u-n func qilish kk)
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';   
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) =>{
+        if(e.target === modal){
+              closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {    // modal ESC bosganda yuqolishi
+        if(e.code === "Escape" && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
 });
