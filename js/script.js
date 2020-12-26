fa7bf604a62d9d4dc7ae4dda69c4692b5602cecb
shaxboz <shaxbozurinov1993@gmail.com>
2020-12-26 14:57:25 +0500

    improve Modal

--- js/script.js
+++ js/script.js
@@ -104,13 +104,16 @@ window.addEventListener('DOMContentLoaded', () =>{
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
     
+    
+    function openModal () {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        //modal.classList.toggle('show');           // 2-usul modalni ochib yopishga
+        document.body.style.overflow = 'hidden';  // modal click qilinganda orqa fon scroll bo'lmasligi uchun
+        clearTimeout(modalTimerId);
+    } 
     modalTrigger.forEach(btn => {                     // trigger 2 ta bugani uchun forEach ishlatildi
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            //modal.classList.toggle('show');           // 2-usul modalni ochib yopishga
-            document.body.style.overflow = 'hidden';  // modal click qilinganda orqa fon scroll bo'lmasligi uchun
-        });
+        btn.addEventListener('click', openModal);
     });
     
     function closeModal () {                        // DRY (2 ta joyda bir xil narsani ishlatmaslig u-n func qilish kk)
@@ -132,4 +135,16 @@ window.addEventListener('DOMContentLoaded', () =>{
             closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+    function showModalByScroll () {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll); // har scroll qilganda chiqmaydi
+        }
+        
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
