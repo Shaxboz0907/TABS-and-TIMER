27624705d5c76c0df29d087bbdca5517442e44a7
shaxboz <shaxbozurinov1993@gmail.com>
2020-12-27 19:39:11 +0500

    class orqali cardni jsda yaratdik

--- js/script.js
+++ js/script.js
@@ -110,7 +110,7 @@ window.addEventListener('DOMContentLoaded', () =>{
         modal.classList.remove('hide');
         //modal.classList.toggle('show');           // 2-usul modalni ochib yopishga
         document.body.style.overflow = 'hidden';  // modal click qilinganda orqa fon scroll bo'lmasligi uchun
-        clearTimeout(modalTimerId);
+        //clearTimeout(modalTimerId);
     } 
     modalTrigger.forEach(btn => {                     // trigger 2 ta bugani uchun forEach ishlatildi
         btn.addEventListener('click', openModal);
@@ -136,7 +136,7 @@ window.addEventListener('DOMContentLoaded', () =>{
         }
     });
 
-    const modalTimerId = setTimeout(openModal, 5000);
+    //const modalTimerId = setTimeout(openModal, 5000);
 
     function showModalByScroll () {
         if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
@@ -147,4 +147,71 @@ window.addEventListener('DOMContentLoaded', () =>{
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+
+    // Используем классы для создание карточек меню
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector){
+            this.src = src;
+            this.alt = alt ;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);    // render metodini joylash uchun ota elm chaqirdik
+            this.transfer = 27;           // kurs qaysidir valyutaga nisbatan kiritiladi
+            this.changeToUAH();       
+        }
+
+        changeToUAH() {              // pulni konvertatsiya qilish metodi
+            this.price = this.price * this.transfer ;
+        }
+
+        render () {                 // htmlga joylashtiradigan metod
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>                      
+            `;
+            this.parent.append(element);      // menu ichidagi containerda append orqali tartib bn elemnet joylashadi 
+        }
+    }
+
+    // const div = new MenuCard();                               // 1- usul
+    // div.render();
+
+    new MenuCard(                    // 1- card                 // 2 -usul chaqirish class ni
+        "img/tabs/vegy.jpg" ,
+        "vegy" ,
+        'Меню "Фитнес"' ,
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'          // this.parent ga berildi! yani menu ichidagi container
+    ).render();
+
+    new MenuCard(                  // 2- card
+        "img/tabs/elite.jpg" ,
+        "elite" ,
+        'Меню “Премиум”' ,
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан! вегетарианских стейков',
+        14,
+        '.menu .container'          // this.parent ga berildi! yani menu ichidagi container
+    ).render();
+
+    new MenuCard(                  // 3- card
+        "img/tabs/post.jpg" ,
+        "post" ,
+        'Меню "Постное"' ,
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container'          // this.parent ga berildi! yani menu ichidagi container
+    ).render();
 });
