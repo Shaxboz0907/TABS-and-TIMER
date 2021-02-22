345f1d6c021fc0b743f1e0efa59c01eebfb75c7b
shaxboz <shaxbozurinov1993@gmail.com>
2021-02-22 17:07:02 +0500

    async/await va db.json bilan ishlash

--- js/script.js
+++ js/script.js
@@ -193,38 +193,45 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // const div = new MenuCard();                               // 1- usul
-    // div.render();
+    const getResource = async (url) =>{  // asixron ishlaydigan func bor dedi (async)
+        const res = await fetch(url); 
 
-    new MenuCard( // 1- card                 // 2 -usul chaqirish class ni
-        "img/tabs/vegy.jpg",
-        "vegy",
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container' // this.parent ga berildi! yani menu ichidagi container
+        if(!res.ok){
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+        return await res.json();
+    };
 
-    ).render();
+    // getResource('http://localhost:3000/menu') // db.jsondan mulumotlarni olamiz
+    //     .then( data => {
+    //         data.forEach(({img, altimg, title , descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });   
 
-    new MenuCard( // 2- card
-        "img/tabs/elite.jpg",
-        "elite",
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан! вегетарианских стейков',
-        14,
-        '.menu .container' // this.parent ga berildi! yani menu ichidagi container
+    getResource('http://localhost:3000/menu') // db.jsondan mulumotlarni olamiz 2-usul verstka u-n 
+        .then(data => createCard(data));
+    
+        function createCard(data){
+            data.forEach(({img, altimg, title , descr, price}) => {
+                const element = document.createElement('div');
 
-    ).render();
+                element.classList.add('menu__item');
 
-    new MenuCard( // 3- card
-        "img/tabs/post.jpg",
-        "post",
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        '.menu .container' // this.parent ga berildi! yani menu ichidagi container
-
-    ).render();
+                element.innerHTML = `
+                    <img src=${img} alt=${altimg}>
+                    <h3 class="menu__item-subtitle">${title}</h3>
+                    <div class="menu__item-descr">${descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                    </div>     
+                
+                `;
+                document.querySelector('.menu .container').append(element);
+            });
+        }
 
     // FORM
 
@@ -236,10 +243,21 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => { // kiritilgan malumotni pastdagi funksiyaga yuborish
-        postData(item);
+        bindPostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) =>{  // asixron ishlaydigan func bor dedi async
+        const res = await fetch(url, {      // await shu func bajarilishini kutish
+            method: 'POST',
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: data              // body = 'formData'ga teng
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -254,20 +272,10 @@ window.addEventListener('DOMContentLoaded', () => {
                    
             const formData = new FormData(form); // form ichidagi (inputlardagi malumotlarni yigish)
 
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries())); // obyektni obyekt qilish
             
-            fetch('server1.php' , {      
-                method: 'POST',
-                headers: {
-                    'Content-type' : 'application/json; charset=utf-8'
-                },
-                body: JSON.stringify(object) // oddiy obyektni json farmatga utkazish 
-                                            // body = 'formData'ga teng
-            })
-            .then(data => data.text()) // server qaytargan malumotni text kurinishida olish
+            postData('http://localhost:3000/requests', json ) 
+            //.then(data => data.text()) // server qaytargan malumotni text kurinishida olish
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);
@@ -304,4 +312,5 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
     }
 
+   
 });
